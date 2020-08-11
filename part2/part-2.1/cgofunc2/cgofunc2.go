cffbe425120a8047b9ab80e941ab44721b7afc59
trainking <yefulincode@gmail.com>
2020-08-11 22:36:52 +0800

    fix cgofunc2

--- part2/part-2.1/cgofunc2/cgofunc2.go
+++ part2/part-2.1/cgofunc2/cgofunc2.go
@@ -1,11 +1,10 @@
 package main
 
+// #include <stdio.h>
+// #include <stdlib.h>
 //#include "hello.h"
 import "C"
 
-/*
-* 只要引入文件和c代码在一个文件夹下，直接用go build一下就可以生成
-*/
 func main() {
 	C.SayHello(C.CString("Hello, World!\n"))
 }
