48999ce335911019cdfc182f8bae09933b23e6c5
trainking <yefulincode@gmail.com>
2020-08-15 23:43:33 +0800

    接口规范RPC

--- part4/part-4.1/rpcClient.go
+++ part4/part-4.1/rpcClient.go
@@ -6,13 +6,39 @@ import (
 	"fmt"
 )
 
+// 名字
+const HelloServiceName = "/path/to/HelloService"
+
+// 定义接口
+type HelloServiceInterface = interface {
+	Hello(request string, reply *string) error
+}
+
+type HelloServiceClient struct {
+	Client *rpc.Client
+}
+
+func DialHelloServiceClient(network, address string) (*HelloServiceClient, error) {
+	c, err := rpc.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	return &HelloServiceClient{Client: c}, nil
+}
+
+func (p *HelloServiceClient) Hello(request string, reply *string) error{
+	return p.Client.Call(HelloServiceName + ".Hello", request, reply)
+}
+
 func main() {
-	client, err := rpc.Dial("tcp", "localhost:1234")
+	// client, err := rpc.Dial("tcp", "localhost:1234")
+	client, err := DialHelloServiceClient("tcp", "localhost:1234")
 	if err != nil {
 		log.Fatal("dialing: ", err)
 	}
 	var reply string
-	err = client.Call("HelloService.Hello", "hello, gg", &reply)
+	// err = client.Call(HelloServiceName + ".Hello", "hello, gg", &reply)
+	err = client.Hello("hello,gg", &reply)
 	if err != nil {
 		log.Fatal(err)
 	}
--- part4/part-4.1/rpchello.go
+++ part4/part-4.1/rpchello.go
@@ -6,6 +6,20 @@ import (
 	"log"
 )
 
+// 名字
+const HelloServiceName = "/path/to/HelloService"
+
+// 定义接口
+type HelloServiceInterface = interface {
+	Hello(request string, reply *string) error
+}
+
+// 注册方法
+func RegisterHelloService(h HelloServiceInterface) error {
+	return rpc.RegisterName(HelloServiceName, h)
+}
+
+// 用结构体去实例化接口
 type HelloService struct {}
 
 func (p *HelloService) Hello(request string, reply *string) error {
@@ -15,7 +29,7 @@ func (p *HelloService) Hello(request string, reply *string) error {
 
 func main()  {
 	// 注册之后监听之后，阻塞等待调用到来，调用一次就会退出
-	rpc.RegisterName("HelloService", new(HelloService))
+	RegisterHelloService(new(HelloService))
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("listene tcp error : ", err)
